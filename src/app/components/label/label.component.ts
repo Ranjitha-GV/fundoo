70dc64e20ccccbc6c0f62c6924725d9b37fc7b31
gourav1995 <gourav.mish1995@gmail.com>
2018-10-30 17:10:08 +0530

    commit

--- src/app/components/label/label.component.ts
+++ src/app/components/label/label.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Inject, Input } from '@angular/core';
+import { Component, OnInit, Inject, Input, ElementRef, ViewChild } from '@angular/core';
 import { FundooNotesComponent } from '../fundoo-notes/fundoo-notes.component';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { HttpService } from '../../services/http.service';
 
 
@@ -11,64 +11,84 @@ import { HttpService } from '../../services/http.service';
 })
 export class LabelComponent implements OnInit {
 
-  constructor(private myHttpService : HttpService) { }
+  public show;
 
-  value1 : any = [];
-    onNoClick(): void {
-    }
-    ngOnInit() 
-  {
-    this.delete(); 
+  constructor(private myHttpService: HttpService) { }
+
+  value1: any = [];
+  @ViewChild('newLabel') newLabel: ElementRef;
+  @ViewChild('myLabel') myLabel: ElementRef;
+
+  onNoClick(): void {
   }
-    id = localStorage.getItem('userId')
-    token = localStorage.getItem('token');
-    addLabel() {
-      this.myHttpService.postNotes('/noteLabels', {
-        "label":document.getElementById('labelId').innerHTML,
-        "isDeleted" : false,
-        "userId": this.id
+  ngOnInit() {
+    this.delete();
+  }
+  id = localStorage.getItem('userId')
+  token = localStorage.getItem('token');
+  addLabel() {
+    this.myHttpService.postNotes('/noteLabels', {
+      "label": this.newLabel.nativeElement.innerHTML,
+      "isDeleted": false,
+      "userId": this.id
 
-      }, this.token).subscribe(
-        (data) => {
-          console.log("POST Request is successful ", data);
-          // this.onNewEntryAdded.emit({
-          // })
-        },
-        error => {
-          console.log("Error", error);
-        })
-    }
-    labelDelete(val) {
-      this.myHttpService.deleteLabel('/noteLabels/'+val+'/deleteNoteLabel', {
-        "label":document.getElementById('labelId').innerHTML
-      }).subscribe(
-        (data) => {
-          console.log("DELETE Request is successful ", data);
-          // this.onNewEntryAdded.emit({
-          // })
-        },
-        error => {
-          console.log("Error", error);
-        })
-    }
-delete()
-{
-  this.myHttpService.getNotes('/noteLabels/getNoteLabelList', this.token).subscribe(
-    (data) => {
-      console.log("GET Request is successful ", data);
-      
-    for(var i = 0; i < data['data']['details'].length; i++ )
-        {
-          if(data['data']['details'][i].isDeleted == false)
-          {
+    }, this.token).subscribe(
+      (data) => {
+        console.log("POST Request is successful ", data);
+        // this.onNewEntryAdded.emit({
+        // })
+      },
+      error => {
+        console.log("Error", error);
+      })
+  }
+  labelDelete(val) {
+    this.myHttpService.deleteLabel('/noteLabels/' + val + '/deleteNoteLabel', {
+      "label": this.newLabel.nativeElement.innerHTML
+    }).subscribe(
+      (data) => {
+        console.log("DELETE Request is successful ", data);
+        // this.onNewEntryAdded.emit({
+        // })
+      },
+      error => {
+        console.log("Error", error);
+      })
+  }
+  delete() {
+    this.myHttpService.getNotes('/noteLabels/getNoteLabelList', this.token).subscribe(
+      (data) => {
+        console.log("GET Request is successful ", data);
+        for (var i = 0; i < data['data']['details'].length; i++) {
+          if (data['data']['details'][i].isDeleted == false) {
             this.value1.push(data['data']['details'][i])
           }
         }
-      console.log(this.value1);
-    },
-    error => {
-      console.log("Error", error);
-    })
-}
-  
+        console.log(this.value1);
+      },
+      error => {
+        console.log("Error", error);
+      })
+  }
+  edit(val) {
+    this.myHttpService.postNotes('/noteLabels/' + val + '/updateNoteLabel',
+      {
+        "label": this.myLabel.nativeElement.innerHTML,
+        "isDeleted": false,
+        "id": val,
+        "userId": localStorage.getItem('userId')
+      },
+      this.token).subscribe(
+        (data) => {
+          console.log("UPDATE Request is successful ", data);
+          console.log(data);
+        },
+        error => {
+          console.log("Error", error);
+        })
+  }
+  edit2(id) {
+    this.show = id;
+  }
+
 }
